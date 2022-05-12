f958911151a69156d3ba230b765d982f1d53824c
xlbrto <hello@xlbrto.com>
2022-05-12 12:08:54 -0700

    Method to round to the smallest value.

---
+++ 8kyu/Keep Hydrated!.js
@@ -0,0 +1,12 @@
+//just a reminder to read the instructions better because it asked to
+//round to the smallest value and for that the MDN is my friend and 
+//Math.floor() was the correct method to use here. 
+
+//solution
+
+function litres(time) {
+    let water = 0.5
+    return Math.floor(time * water)
+  }
+  
+  litres(11.8)
