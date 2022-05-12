0f49d6ec50453039ec464d19e1705dbf800109b3
xlbrto <hello@xlbrto.com>
2022-05-11 18:43:12 -0700

    Function squares an argument.

---
+++ 8kyu/Squaring an argument.js
@@ -0,0 +1,11 @@
+//I tried using Math.pow() but it wasn't the correct method
+//so I figured I just had to multiply the number by itself to
+//get the requested instruction.
+
+//solution
+
+function square(input){
+    return input * input
+  }
+  
+  square(3) 
