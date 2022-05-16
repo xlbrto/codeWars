9fb767f53f8c8883026c0ea5772c84d64384cad0
xlbrto <hello@xlbrto.com>
2022-05-16 09:18:59 -0700

    Multiplies two numbers.

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,13 @@
+// this code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//     return a * b
+//   }
+  
+//solution 
+
+function multiply(a, b){ 
+    let testOne = a * b  
+    return testOne 
+  } 
+  multiply (3,5)  
