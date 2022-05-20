a5b89beca0bf0c344ea9b674ce0ade1e6759f091
xlbrto <hello@xlbrto.com>
2022-05-19 19:13:34 -0700

    Find the correct method to multiply a string.

---
+++ 8kyu/String repeat.js
@@ -0,0 +1,9 @@
+//had to figure out which method was going to give me the expected output
+//in this case it was the "string".repeat() method x (n), which is how many times we're repeating
+//the string
+
+//solution
+
+function repeatStr(n,s){
+    return s.repeat(n)
+  }
