99c51deacb7157845ec62a50d972614117242f1c
xlbrto <hello@xlbrto.com>
2022-05-20 18:08:23 -0700

    Convert the given boolean into a string.

---
+++ 8kyu/Convert a boolean to string.js
@@ -0,0 +1,11 @@
+//I used an "if else" loop to get to the expected output
+
+//solution
+
+function booleanToString(b){
+    if(b){
+      return "true"
+    }else{
+      return "false"
+    }
+  }
