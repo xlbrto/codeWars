163694390d3522083752be882d7f93f5f20b972e
xlbrto <hello@xlbrto.com>
2022-05-22 11:13:05 -0700

    Function turns string to uppercase.

---
+++ 8kyu/Make uppercase.js
@@ -0,0 +1,8 @@
+//Finding a method that turns the input string to uppercase
+//toUpperCase() method was needed to get to the expected output
+
+//solution
+
+function makeUpperCase(str){
+    return str.toUpperCase()
+  }
