f999650b5bfe7d63d9eb1054154f11d672af3999
xlbrto <hello@xlbrto.com>
2022-05-22 10:57:00 -0700

    Function gives a personalized greeting.

---
+++ 8kyu/Grasshopper.js
@@ -0,0 +1,11 @@
+//Instruction requiered an "if else" conditional to get to the expected outcome
+//case name equals owner "do x", otherwise "do y".
+
+//solution
+
+function greet (name, owner){
+    if(name === owner){
+      return 'Hello boss'
+        }else
+          return 'Hello guest'
+  }
