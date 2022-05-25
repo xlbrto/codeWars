224b9cf8a28e6f15037b78b34fa97686e99ad607
xlbrto <hello@xlbrto.com>
2022-05-24 22:41:36 -0700

    Function squares every single digit.

---
+++ 7kyu/Square every digit.js
@@ -0,0 +1,10 @@
+//solution
+
+function squareDigits(num){
+    let string = num.toString()
+    let results = []
+    for (let i = 0; i < string.length; i++){
+        results[i] = string[i] * string[i]
+    }
+    return Number(results.join(''))
+}
