f94cc6fbd1e15c6a792941d85d6d02dda542c760
xlbrto <hello@xlbrto.com>
2022-05-25 20:23:57 -0700

    Function turns string to number.

---
+++ 8kyu/Convert a string to a number.js
@@ -0,0 +1,9 @@
+//Instructions required to turn a string into a number and I decided to use
+//Number() method but I could've used parseInt() or parseFloat() to achieve the
+//same result
+
+//solution
+
+const stringToNumber = function(str){
+    return Number(str)
+  }
