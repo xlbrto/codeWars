76a7c7a4f67ab0a9817a22406bb36312e2fe2c98
xlbrto <hello@xlbrto.com>
2022-05-25 20:40:36 -0700

    Function returns negative number.

---
+++ 8kyu/Return negative.js
@@ -0,0 +1,8 @@
+//Instructions required to turn a number to it's negative counterpart and I
+//decided to use -Math.abs() method to solve it
+
+//solution
+
+function makeNegative(num){
+    return -Math.abs(num)
+  }
