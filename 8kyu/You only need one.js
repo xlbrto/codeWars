aa7109e8b8f135325fcda5223015182f025769b7
xlbrto <hello@xlbrto.com>
2022-05-31 20:15:03 -0700

    Function checks an array for a specific value.

---
+++ 8kyu/You only need one.js
@@ -0,0 +1,8 @@
+//Instructions required to use the .includes() method to complete it, 
+// which checks if an array includes a certain value in its entries
+
+//solution
+
+function check(a, x){
+    return a.includes(x)
+  }
