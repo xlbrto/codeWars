edbcaf86b08fd5aa8898bd46eb6e788418a811d6
xlbrto <hello@xlbrto.com>
2022-06-01 22:52:38 -0700

    Function replaces thymine for uracil.

---
+++ 8kyu/DNA to RNA.js
@@ -0,0 +1,8 @@
+//Instructions required the replace() method to switch the letter T to U and 
+//a regular expression with global search inside
+
+//solution
+
+function DNAtoRNA(dna){
+    return dna.replace(/T/g,'U')
+  }
